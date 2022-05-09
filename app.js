613c07f57431a894ad343eec5f5badbea23bb731
oshanedesign <oshane.design@gmail.com>
2022-05-08 20:26:13 -0500

    make text bold upon arrow click

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 // Logic
 function result1() {
   const toggleAble = document.querySelector('.a');
+  const boldText = document.querySelector('.light1');
+
   if (toggleAble.id === 'down-arrow1') {
     toggleAble.classList.toggle('fa-angle-up');
+    boldText.style.fontWeight = 'bold';
   }
 }
 
 function result2() {
   const toggleAble = document.querySelector('.b');
+  const boldText = document.querySelector('.light2');
+
   if (toggleAble.id === 'down-arrow2') {
     toggleAble.classList.toggle('fa-angle-up');
+    boldText.style.fontWeight = 'bold';
   }
 }
 
 function result3() {
   const toggleAble = document.querySelector('.c');
+  const boldText = document.querySelector('.light3');
+
   if (toggleAble.id === 'down-arrow3') {
     toggleAble.classList.toggle('fa-angle-up');
+    boldText.style.fontWeight = 'bold';
   }
 }
 
 function result4() {
   const toggleAble = document.querySelector('.d');
+  const boldText = document.querySelector('.light4');
+
   if (toggleAble.id === 'down-arrow4') {
     toggleAble.classList.toggle('fa-angle-up');
+    boldText.style.fontWeight = 'bold';
   }
 }
 
 function result5() {
   const toggleAble = document.querySelector('.e');
+  const boldText = document.querySelector('.light5');
+
   if (toggleAble.id === 'down-arrow5') {
     toggleAble.classList.toggle('fa-angle-up');
+    boldText.style.fontWeight = 'bold';
   }
 }
 
